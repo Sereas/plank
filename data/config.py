79dcc4e817c3312854cb9b585933726c0883dcaa
Oleg Dylevich <oleg.dylevich@gmail.com>
2021-11-02 17:31:17 +0300

    testing commit from pycharm

--- data/config.py
+++ data/config.py
@@ -4,7 +4,9 @@ from environs import Env
 env = Env()
 env.read_env()
 
-BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
-ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
+#BOT_TOKEN = str("2014222904:AAHNsExPuBAjWJ6SsX2otjwOp6WVz91u_Gg")
+#ADMINS = str("327154479")  # Тут у нас будет список из админов
+BOT_TOKEN = env.str("BOT_TOKEN") # Забираем значение типа str BOT_TOKEN
+ADMINS = env.str("ADMINS")  # Тут у нас будет список из админов
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
 
--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -6,9 +6,10 @@ from data.config import ADMINS
 
 
 async def on_startup_notify(dp: Dispatcher):
-    for admin in ADMINS:
-        try:
-            await dp.bot.send_message(admin, "Бот Запущен")
+    admin = ADMINS
+    try:
+        print('admin', admin)
+        await dp.bot.send_message(admin, "Бот Запущен")
 
-        except Exception as err:
-            logging.exception(err)
+    except Exception as err:
+        logging.exception(err)
