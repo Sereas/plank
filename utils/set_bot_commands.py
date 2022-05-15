b625e086ae3b3e7ce8ef6273f80f54cbb16641e2
Sereas <65492966+Sereas@users.noreply.github.com>
2022-05-15 15:56:58 +0300

    Latest updates 15/05/22 - minor fixes for first deploy

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -5,6 +5,6 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
             types.BotCommand("menu", "Вывести меню"),
-            types.BotCommand("test", "Launch test"),
+            #types.BotCommand("test", "Launch test"),
         ]
     )
